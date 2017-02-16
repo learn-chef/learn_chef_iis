056c1d0e95f65d4f09d6c0112381ef9ff041e4bc
Thomas Petchel <tpetchel@chef.io>
2017-02-16 09:14:21 -0500

    Roll back version
    As a teaching tool, the version can stay at 0.1.0, even if we update things.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures learn_chef_iis'
 long_description 'Installs/Configures learn_chef_iis'
-version '0.2.0'
+version '0.1.0'
 issues_url 'https://github.com/learn-chef/learn_chef_iis/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/learn-chef/learn_chef_iis' if respond_to?(:source_url)
